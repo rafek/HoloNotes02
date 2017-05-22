27a1aebfa915d8699e3951e5afd62f7c1fb722f2
rafek <rafal.legiedz@gmail.com>
2017-05-22 21:56:13 +0200

    First try with Unity UI.

--- Assets/PlacementManager.cs
+++ Assets/PlacementManager.cs
@@ -7,7 +7,10 @@ public class PlacementManager : MonoBehaviour, IInputClickHandler {
     [Tooltip("Prefab for notes objects.")]
     public GameObject NotePrefab;
 
+    public GameObject NoteInputCanvas;
+
     private int _spatialMappingLayerID;
+    private GameObject _newNote;
 
     public void OnInputClicked(InputClickedEventData eventData)
     {
@@ -17,9 +20,25 @@ public class PlacementManager : MonoBehaviour, IInputClickHandler {
             {
                 var newNotePosition = GazeManager.Instance.HitInfo.point;
 
-                var newNote = Instantiate(NotePrefab, newNotePosition, Quaternion.identity);
+                _newNote = Instantiate(NotePrefab, newNotePosition, Quaternion.identity);
+
+                NoteInputCanvas.SetActive(true);
             }
         }
+    }
+
+    public void OnConfirmPlacement()
+    {
+        
+    }
+
+    public void OnCancelPlacement()
+    {
+        Destroy(_newNote);
+
+        _newNote = null;
+
+        NoteInputCanvas.SetActive(false);
     }
 
     private void Awake()
