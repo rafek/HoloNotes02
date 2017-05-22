c3bf1778df25f61055171adb784e212c3148d195
rafek <rafal.legiedz@gmail.com>
2017-05-22 10:28:02 +0200

    Initial commit with a very standard behaviour - putting sphere (notes) around.

---
+++ Assets/PlacementManager.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using HoloToolkit.Unity.InputModule;
+using UnityEngine;
+
+public class PlacementManager : MonoBehaviour, IInputClickHandler {
+
+    [Tooltip("Prefab for notes objects.")]
+    public GameObject NotePrefab;
+
+    private int _spatialMappingLayerID;
+
+    public void OnInputClicked(InputClickedEventData eventData)
+    {
+        if (GazeManager.Instance.IsGazingAtObject)
+        {
+            if (GazeManager.Instance.HitObject.layer == _spatialMappingLayerID)
+            {
+                var newNotePosition = GazeManager.Instance.HitInfo.point;
+
+                var newNote = Instantiate(NotePrefab, newNotePosition, Quaternion.identity);
+            }
+        }
+    }
+
+    private void Awake()
+    {
+        _spatialMappingLayerID = LayerMask.NameToLayer("SpatialMapping");
+    }
+
+    private void Start()
+    {
+        InputManager.Instance.PushFallbackInputHandler(gameObject);
+    }
+
+
+}
